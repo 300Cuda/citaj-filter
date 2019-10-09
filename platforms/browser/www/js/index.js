49bf3b5f16e76f9df8a306adbff0a5941b464271
300Cuda <42349675+300Cuda@users.noreply.github.com>
2019-10-09 17:32:53 +0200

    a
    a

---
+++ platforms/browser/www/js/index.js
@@ -0,0 +1,23 @@
+
+var app = {
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+
+    onDeviceReady: function() {
+        app.receivedEvent('deviceready');
+		
+    },
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        var ekran = document.getElementById("ekran");
+		cordova.InAppBrowser.open('https://www.citajfilter.com/test','_self', 'toolbar=no,location=no,status=no,menubar=no,zoom=no');
+		
+
+    }
+};
