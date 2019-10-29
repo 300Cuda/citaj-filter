9de78216bd18c86e9aeec830375b82d30bedd6d3
300Cuda <42349675+300Cuda@users.noreply.github.com>
2019-10-29 17:05:21 +0100

    brrr

--- www/js/index.js
+++ www/js/index.js
@@ -7,6 +7,7 @@ window.onerror = function(msg, url, linenumber) {
  
  function otherShare(url){
 	 window.plugins.socialsharing.share(null, null,null,url);
+	 localStorage.setItem(key,'');
   };
 var app = {
     // Application Constructor
@@ -40,7 +41,7 @@ var app = {
 		  // 1st Clear out 'hidden' in localStorage for subsequent opens.
 		  // 2nd Create the button
 		  ref.executeScript({
-			code: "var key = 'hidden'; var keyval = 'yes'; localStorage.setItem('hidden',''); var button = document.createElement('Button'); button.innerHTML = 'Share'; button.style = 'top:0;right:0;position:fixed;'; document.body.appendChild(button); button.setAttribute('onclick','localStorage.setItem(key,keyval);');"
+			code: "var key = 'hidden'; var keyval = 'yes'; localStorage.setItem('hidden',''); var button = document.createElement('Button'); button.innerHTML = 'Share'; button.style = 'top:0;right:0;position:fixed;color:#fcbc50;'; document.body.appendChild(button); button.setAttribute('onclick','localStorage.setItem(key,keyval);');"
 		  });
 
 		  // Start an interval
@@ -57,9 +58,6 @@ var app = {
 				// If 'hidden' is equal to 'yes', clear the interval and hide the InAppBrowser.
 				if (hidden === 'yes') {
 					otherShare(trenutni_url);
-					ref.executeScript({
-						code: "localStorage.setItem('hidden','')"
-					},
 					clearInterval(loop);
 				  
 				}
