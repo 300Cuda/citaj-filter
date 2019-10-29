35500de3c4b3229dcba3c81c70d982716b7081c4
300Cuda <42349675+300Cuda@users.noreply.github.com>
2019-10-29 14:38:45 +0100

    uuuu

--- www/js/index.js
+++ www/js/index.js
@@ -35,7 +35,7 @@ var app = {
 		  // 1st Clear out 'hidden' in localStorage for subsequent opens.
 		  // 2nd Create the button
 		  ref.executeScript({
-			code: "var button = document.createElement('Button'); button.innerHTML = 'Share'; button.style = 'top:0;right:0;position:fixed;'; document.body.appendChild(button); button.setAttribute('onclick','alert('tu')');"
+			code: "var key = 'hidden'; var keyval = 'yes'; localStorage.setItem('hidden',''); var button = document.createElement('Button'); button.innerHTML = 'Share'; button.style = 'top:0;right:0;position:fixed;'; document.body.appendChild(button); button.setAttribute('onclick','otherShare();');"
 		  });
 
 		  // Start an interval
