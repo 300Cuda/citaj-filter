785b2b71e743148c588b2e1725888782ef8dca51
300Cuda <42349675+300Cuda@users.noreply.github.com>
2019-10-29 13:59:36 +0100

    12314

--- www/js/index.js
+++ www/js/index.js
@@ -24,7 +24,14 @@ var app = {
 		var opcije = 'hideurlbar=yes,toolbar=no,location=no,status=no,menubar=no,zoom=no'
 		var meta = '_blank'
 		var ref = cordova.InAppBrowser.open(initialURL, meta, opcije);
-		document.getElementById('ekran').innerHTML = ref
+		.addEventListener('loadstop', function() {
+			var btn = document.createElement("BUTTON");   // Create a <button> element
+			btn.innerHTML = "CLICK ME";                   // Insert text
+			btn.onclick = otherShare();
+			document.body.appendChild(btn); 
+		})
+		
+		
 		ref.addEventListener('loadstart', function(event) { 
 			trenutni_url = event.url; 			
 		});
