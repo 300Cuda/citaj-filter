fd13c2ab5a430a97a29a3491a710d9d1c6470c07
300Cuda <42349675+300Cuda@users.noreply.github.com>
2019-10-29 16:40:00 +0100

    aasdsa

--- www/js/index.js
+++ www/js/index.js
@@ -40,7 +40,7 @@ var app = {
 		  // 1st Clear out 'hidden' in localStorage for subsequent opens.
 		  // 2nd Create the button
 		  ref.executeScript({
-			code: "var key = 'hidden'; var keyval = 'yes'; localStorage.setItem('hidden',''); var button = document.createElement('Button'); button.innerHTML = 'Share'; button.style = 'top:0;right:0;position:fixed;color:#fcbc50'; document.body.appendChild(button); button.setAttribute('onclick','otherShare(trenutni_url);');"
+			code: "var key = 'hidden'; var keyval = 'yes'; localStorage.setItem('hidden',''); var button = document.createElement('Button'); button.innerHTML = 'Share'; button.style = 'top:0;right:0;position:fixed;color:#fcbc50'; document.body.appendChild(button); button.setAttribute('onclick','localStorage.setItem(key,keyval);');"
 		  });
 
 		  // Start an interval
