91f3b81e7f282c0bbb9ac1ba7a71bdac31b70afe
300Cuda <42349675+300Cuda@users.noreply.github.com>
2019-10-29 12:20:21 +0100

    aaa

--- www/js/index.js
+++ www/js/index.js
@@ -11,15 +11,16 @@ var app = {
 
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
-		
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
 		var initialURL = 'https://www.citajfilter.com'
 		var opcije = 'hideurlbar=yes,toolbar=no,location=no,status=no,menubar=no,zoom=no'
 		var meta = '_blank'
 		var ref = cordova.InAppBrowser.open(initialURL, meta, opcije);
 		ref.addEventListener('loadstart', function() { alert(event.url); });
+		
+    },
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+
 
     }
 
