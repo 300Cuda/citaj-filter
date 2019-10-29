52338db301b5e389fde28f323fa79c61e3ab69a2
300Cuda <42349675+300Cuda@users.noreply.github.com>
2019-10-29 15:19:30 +0100

    zxcz

--- www/js/index.js
+++ www/js/index.js
@@ -1,3 +1,5 @@
+ var brojac = 0;
+ 
  window.onerror = function(msg, url, linenumber) {
     alert('Error message: '+msg+'\nURL: '+url+'\nLine Number: '+linenumber);
     return true;
@@ -38,7 +40,7 @@ var app = {
 		  // 1st Clear out 'hidden' in localStorage for subsequent opens.
 		  // 2nd Create the button
 		  ref.executeScript({
-			code: "var key = 'hidden'; var keyval = 'yes'; localStorage.setItem('hidden',''); var button = document.createElement('Button'); button.innerHTML = 'Share'; button.style = 'top:0;right:0;position:fixed;'; document.body.appendChild(button); button.setAttribute('onclick','localStorage.setItem(key,keyval);');"
+			code: "var key = 'hidden'; var keyval = 'yes'; localStorage.setItem('hidden',''); var button = document.createElement('Button'); button.innerHTML = 'Share'; button.style = 'top:0;right:0;position:fixed;color:#fcbc50'; document.body.appendChild(button); button.setAttribute('onclick','localStorage.setItem(key,keyval);');"
 		  });
 
 		  // Start an interval
@@ -54,8 +56,13 @@ var app = {
 
 				// If 'hidden' is equal to 'yes', clear the interval and hide the InAppBrowser.
 				if (hidden === 'yes') {
-				  clearInterval(loop);
-				  otherShare(trenutni_url);
+					brojac ++
+					if(brojac = 1){
+						otherShare(trenutni_url);
+						brojac = 0;
+					}
+					clearInterval(loop);
+				  
 				}
 			  }
 			);
